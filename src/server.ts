8800854b0d1acec634c86af4bafc4518addc4168
Gabe Kuslansky MacbookPro <kuslanskyg@gmail.com>
2023-01-22 15:22:54 -0500

    try new cors

--- src/server.ts
+++ src/server.ts
@@ -26,21 +26,12 @@ export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(
   cors({
-    credentials: true,
+    // credentials: true,
     origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
 );
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 const environment = process.env.NODE_ENV === "production" ? "prod" : "dev";
 console.log("Frontend URL:", FRONTEND_URL);
 app.listen(port, () => {
