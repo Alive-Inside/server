b440b843f4ce295c762c52d4e5ff12bb164a38f6
Gabe Kuslansky MacbookPro <kuslanskyg@gmail.com>
2023-02-06 17:35:30 -0500

    add logging

--- src/api/email.ts
+++ src/api/email.ts
@@ -1,13 +1,16 @@
 import { Request, Response, Router } from "express";
 import { sendSongListEmail } from "../email";
+import _ from "lodash";
 
 const router = Router();
 
 router.post("/email", async (req: Request, res: Response) => {
+  console.log("Hit email");
   console.log(req.body);
   const { emails, formQuestionsAndAnswers } = req.body;
   console.log(formQuestionsAndAnswers);
-  for (const email of emails) {
+  console.log(_.uniq(["mrb@aliveinside.org", ...emails]));
+  for (const email of _.uniq(["mrb@aliveinside.org", ...emails])) {
     console.log("sending email to ", email);
     console.log(JSON.stringify(formQuestionsAndAnswers));
     await sendSongListEmail(email, formQuestionsAndAnswers);
