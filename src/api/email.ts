6f2b05821bb4f893690ecdaeb95f3a2a2ee03faa
Gabe Kuslansky MacbookPro <kuslanskyg@gmail.com>
2023-02-07 16:26:26 -0500

    api

--- src/api/email.ts
+++ src/api/email.ts
@@ -5,22 +5,26 @@ import _ from "lodash";
 const router = Router();
 
 router.post("/email", async (req: Request, res: Response) => {
-  console.log("Hit email");
-  console.log(req.body);
-  const { emails, formQuestionsAndAnswers } = req.body;
-  console.log(formQuestionsAndAnswers);
-  const emailsWithAdmin =
-    process.env.NODE_ENV === "production"
-      ? _.uniq(["mrb@aliveinside.org", ...emails])
-      : emails;
-  console.log(emailsWithAdmin);
-  for (const email of emailsWithAdmin) {
-    console.log("sending email to ", email);
-    console.log(JSON.stringify(formQuestionsAndAnswers));
-    await sendSongListEmail(email, formQuestionsAndAnswers);
-    console.log("email sent");
+  try {
+    console.log("Hit email");
+    console.log(req.body);
+    const { emails, formQuestionsAndAnswers } = req.body;
+    console.log(formQuestionsAndAnswers);
+    const emailsWithAdmin =
+      process.env.NODE_ENV === "production"
+        ? _.uniq(["mrb@aliveinside.org", ...emails])
+        : emails;
+    console.log(emailsWithAdmin);
+    for (const email of emailsWithAdmin) {
+      console.log("sending email to ", email);
+      console.log(JSON.stringify(formQuestionsAndAnswers));
+      await sendSongListEmail(email, formQuestionsAndAnswers);
+      console.log("email sent");
+    }
+    res.sendStatus(200);
+  } catch (e) {
+    res.sendStatus(500);
   }
-  res.status(200);
 });
 
 export default router;
